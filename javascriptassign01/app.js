cfadede349300604ef62f40839a3fe89165dbf88
WardahIrshad <wardahirshad117@gmail.com>
2024-09-19 20:52:36 +0500

    Add files via upload

---
+++ javascriptassign01/app.js
@@ -0,0 +1,42 @@
+// ***** QNO 01 *****
+
+ alert("WARDAH IRSHAD");
+
+ //  ...End...
+
+//***** QNO 02 *****
+
+var name = "Wardah Khan";
+
+alert("My name is" +" " + name)
+
+ console.log(name)
+
+// ...End ...
+
+//***** QNO 03 *****
+
+
+var a = 4;
+var b = 2;
+ var result = (a++ + --b - --a + b++ + a + b)
+
+ console.log(result)
+
+
+// ...End ...
+
+// ***** QNO 04 *****
+ 
+var age = prompt("How old are you?");
+if(age<=15){
+    alert("you are a Matric Student");
+    
+} else if (age>=17 && age<=30){
+    alert("you are a university student")
+
+} else{
+    alert("you are not a student")
+}
+
+// ...End ...
